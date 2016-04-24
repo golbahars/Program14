8960c481cf7b902695d89d3bdc4a6c85e87788c1
John Shekleton <shekletojo@mnstate.edu>
2016-04-23 23:20:56 -0500

    added __init__

--- p14Test.py
+++ p14Test.py
@@ -1,1 +1,7 @@
-
+class CustomSet:
+    def __init__(self,tmpList):
+        for a in tmpList:
+            while tmpList.count(a) > 1:
+                tmpList.remove(a)
+        self._setList = tmpList
+        
