6ec8e5c87623f5505814fe99f6abc1643086d90e
golbahars <sadatrezgo@mnstate.edu>
2016-04-25 17:31:55 -0500

    Update modCustomSet.py

--- modCustomSet.py
+++ modCustomSet.py
@@ -10,7 +10,7 @@ def __sub___(self,other):
     return self._setList - other._setList
 
 def membership(self,element):
-     '''checks if the element is a member of the set
+    '''checks if the element is a member of the set
        precondition: element is a type int
        postcondition: a type Bool is returned
     '''
@@ -19,9 +19,9 @@ def membership(self,element):
     else:
         return False
 
-def __ge__(self,other):
+def __le__(self,other):
     '''checks if a set is the subset of another set
        precondition: other is a type CustomeSet
        postcondition: a type Bool is returned
     '''
-    return self._setList >= other._setList
+    return self._setList <= other._setList
