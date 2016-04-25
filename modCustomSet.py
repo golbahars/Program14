a10f45035512a0a178687077fc631901c0d337e5
Golbahar Sadatrezaei <kl7856yo@mnstate.edu>
2016-04-25 17:13:33 -0500

    updated

--- modCustomSet.py
+++ modCustomSet.py
@@ -1,1 +1,27 @@
+''' golbahar's methods'''
 
+def __sub___(self,other):
+    '''retuens a new set with elements in first set but not the second
+       precondition: other is a type CustomeSet
+       postcondition: None
+    '''
+       
+
+    return self._setList - other._setList
+
+def membership(self,element):
+     '''checks if the element is a member of the set
+       precondition: element is a type int
+       postcondition: a type Bool is returned
+    '''
+    if element in self._setList:
+        return True
+    else:
+        return False
+
+def __ge__(self,other):
+    '''checks if a set is the subset of another set
+       precondition: other is a type CustomeSet
+       postcondition: a type Bool is returned
+    '''
+    return self._setList >= other._setList
