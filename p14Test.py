1b5314e0fe4326ab2489d0391edbd61f835c3545
aarnessda <aarnessda@mnstate.edu>
2016-04-26 18:39:22 -0400

    Update p14Test.py

--- p14Test.py
+++ p14Test.py
@@ -1,7 +1,8 @@
-class CustomSet:
-    def __init__(self,tmpList):
-        for a in tmpList:
-            while tmpList.count(a) > 1:
-                tmpList.remove(a)
-        self._setList = tmpList
+
         
+set1 = CustomSet([1,3,1])
+set2 = CustomSet([1,2,3,4,4])
+result = set1>=set2
+print("Is set1 a superset of set2?:",result)
+result = set2>=set1
+print("is set2 a superset of set1?:",result)
