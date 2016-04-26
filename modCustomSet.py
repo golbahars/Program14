e95c4448cc745f974ce6ddd1c0aeb1612b66229e
aarnessda <aarnessda@mnstate.edu>
2016-04-26 18:38:35 -0400

    Update modCustomSet.py

--- modCustomSet.py
+++ modCustomSet.py
@@ -1,3 +1,16 @@
 class CustomSet:
     def __init__:(self, tmpList):
         self._setList = [a for a in tmpList if a not in self._setList]
+    def __gt__(self, tmpSet):
+        """Description: checks if tmpSet is a superset of self
+        PreCond: self(list),tmpSet(list)
+        PostCond: returns true if it is a superset. returns false if not.
+        """
+        numct = 0
+        for ct in range(0,len(self)):
+            if self[ct] in tmpSet:
+                numct +=1
+        if ct == len(self):
+            return True
+        else:
+            return False
