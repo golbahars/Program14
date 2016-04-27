821effe415e25ecb48278c97754c755fa78bfa93
JohnShekleton <jts15shek@gmail.com>
2016-04-26 20:33:49 -0500

    Update modCustomSet.py

--- modCustomSet.py
+++ modCustomSet.py
@@ -1,3 +1,21 @@
 class CustomSet:
-    def __init__:(self, tmpList):
-        self._setList = [a for a in tmpList if a not in self._setList]
+    def __init__(self,tmpList):
+        for a in tmpList:
+            while tmpList.count(a) > 1:
+                tmpList.remove(a)
+        self._setList = tmpList
+    
+    def __add__(self,other):
+        for a in other:
+            self._setList.append(a)
+        newList = CustomSet.__init__(self._setList) 
+        return newList
+    
+    def __and__(self,other):
+        newList = self._setList
+        for a in other:
+            if a not in newList:
+                newList.remove(a)
+        return newList
+        
+                
