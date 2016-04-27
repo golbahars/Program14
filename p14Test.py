c7c5560e29e143d83f1d4a5d753b2a1e04554c85
JohnShekleton <jts15shek@gmail.com>
2016-04-26 20:19:52 -0500

    Update p14Test.py

--- p14Test.py
+++ p14Test.py
@@ -1,7 +1,1 @@
-class CustomSet:
-    def __init__(self,tmpList):
-        for a in tmpList:
-            while tmpList.count(a) > 1:
-                tmpList.remove(a)
-        self._setList = tmpList
-        
+
